d5f922ba90bf5156c1f7a3a33349616317bf9689
Daniele Longo <dlongodesign@gmail.com>
2023-06-25 17:34:07 -0400

    simplified navRoutes code

--- src/lib/components/NavbarScenes/NavRoutes.svelte
+++ src/lib/components/NavbarScenes/NavRoutes.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import { drawerStore } from '@skeletonlabs/skeleton';
 
+	const navLinks = [
+		{
+			title: 'About',
+			href: '/about'
+		},
+		{
+			title: 'Menu',
+			href: '/menu'
+		},
+		{
+			title: 'Services',
+			href: '/services'
+		},
+		{
+			title: 'Contact',
+			href: '/contact'
+		}
+	];
+
 	function drawerClose(): void {
 		drawerStore.close();
 	}
@@ -10,34 +29,15 @@
 	<i class="fas fa-times" />
 </button>
 <ul class="mt-20 lg:mt-0 z-[10] flex flex-col lg:flex-row uppercase text-lg">
-	<li class="p-4 lg:p-0 hover:bg-primary-400 lg:hover:bg-transparent">
-		<a
-			class="px-8 pb-1 lg:hover:border-b-8 lg:hover:border-primary-500 lg:hover:transition-all"
-			on:click={drawerClose}
-			href="/about">About</a
-		>
-	</li>
-	<li class="p-4 lg:p-0 hover:bg-primary-400 lg:hover:bg-transparent">
-		<a
-			class="px-8 pb-1 lg:hover:border-b-8 lg:hover:border-primary-500 lg:hover:transition-all"
-			on:click={drawerClose}
-			href="/menu">Menu</a
-		>
-	</li>
-	<li class="p-4 lg:p-0 hover:bg-primary-400 lg:hover:bg-transparent">
-		<a
-			class="px-8 pb-1 lg:hover:border-b-8 lg:hover:border-primary-500 lg:hover:transition-all"
-			on:click={drawerClose}
-			href="/services">Services</a
-		>
-	</li>
-	<li class="p-4 lg:p-0 hover:bg-primary-400 lg:hover:bg-transparent">
-		<a
-			class="px-8 pb-1 lg:hover:border-b-8 lg:hover:border-primary-500 lg:hover:transition-all"
-			on:click={drawerClose}
-			href="/contact">Contact</a
-		>
-	</li>
+	{#each navLinks as { title, href }}
+		<li class="p-4 lg:p-0 hover:bg-primary-400 lg:hover:bg-transparent">
+			<a
+				class="px-8 pb-1 lg:hover:border-b-8 lg:hover:border-primary-500 lg:hover:transition-all"
+				on:click={drawerClose}
+				{href}>{title}</a
+			>
+		</li>
+	{/each}
 </ul>
 <div class="lg:hidden mt-4 px-8">
 	<a class="btn variant-ghost-surface text-surface-50 uppercase" href="/menu">Place an Order</a>
