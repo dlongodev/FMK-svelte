b388c1e4eb817108d397b92f7ee0b19a46f95d76
Daniele Longo <dlongodesign@gmail.com>
2023-06-25 18:57:16 -0400

    Fix naming conventions for components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import Header from '$lib/components/Home/Header.svelte';
-	import Section1 from '$lib/components/Home/Section1.svelte';
-	import Section2 from '$lib/components/Home/Section2.svelte';
+	import HeroSection from '$lib/components/Home/HeroSection.svelte';
+	import SectionOne from '$lib/components/Home/SectionOne.svelte';
+	import SectionTwo from '$lib/components/Home/SectionTwo.svelte';
 </script>
 
-<Header />
-<Section1 />
-<Section2 />
+<HeroSection />
+<SectionOne />
+<SectionTwo />
