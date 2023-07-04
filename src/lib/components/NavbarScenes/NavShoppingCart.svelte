2da29ae671c106898aab162b0d25af2917a53f7b
Daniele Longo <dlongodesign@gmail.com>
2023-07-03 23:30:51 -0400

    add cart drawer items

--- src/lib/components/NavbarScenes/NavShoppingCart.svelte
+++ src/lib/components/NavbarScenes/NavShoppingCart.svelte
@@ -4,21 +4,36 @@
 	const drawerSettings: DrawerSettings = {
 		id: 'cart-drawer',
 		position: 'right',
-		// bgBackdrop: 'bg-gradient-to-tr from-primary-500/50 to-primary-500/50',
-		width: 'w-[280px] md:w-[380px]',
+		width: 'w-full sm:max-w-[540px]',
 		blur: 'backdrop-blur-sm'
 	};
 
 	function drawerOpen(): void {
 		drawerStore.open(drawerSettings);
 	}
+	const cart = {
+		cartItems: [
+			{
+				title: 'Chicken Parmesan',
+				quantity: 1,
+				price: 60
+			},
+			{
+				title: 'Chicken Francese',
+				quantity: 1,
+				price: 60
+			}
+		],
+		totalQuantity: 2,
+		total: 120
+	};
 </script>
 
 <button on:click={drawerOpen} class="btn-icon variant-soft text-primary-500 ml-5 relative">
 	<div
 		class="absolute -top-1 -right-1 w-5 h-5 rounded-full p-1 flex items-center justify-center bg-white text-primary-500 font-semibold"
 	>
-		0
+		{cart.totalQuantity}
 	</div>
 	<i class="fa-solid fa-cart-shopping text-xl !m-0" />
 </button>
--- src/lib/components/cart/CartDrawer.svelte
+++ src/lib/components/cart/CartDrawer.svelte
@@ -4,11 +4,87 @@
 	function drawerClose(): void {
 		drawerStore.close();
 	}
+
+	const cart = {
+		cartItems: [
+			{
+				title: 'Chicken Parmesan',
+				quantity: 1,
+				price: 60
+			},
+			{
+				title: 'Chicken Francese',
+				quantity: 1,
+				price: 60
+			}
+		],
+		totalQuantity: 2,
+		total: 120
+	};
 </script>
 
-<div class="text-right">
-	<button type="button" on:click={drawerClose} class="btn-icon variant-filled-surface m-4">
-		<i class="fas fa-times" />
-	</button>
-</div>
-<h1>cart content</h1>
+<section class="h-full flex flex-col justify-between">
+	<div class="p-4 flex items-center justify-between bg-primary-500 text-white">
+		<h1 class="text-xl">Cart Summary</h1>
+		<button type="button" on:click={drawerClose} class="btn-icon variant-filled-primary">
+			<i class="fas fa-times" />
+		</button>
+	</div>
+	<div class="h-full p-4">
+		<ul class="divide-y divide-primary-200">
+			{#each cart.cartItems as item}
+				<li class="flex flex-col py-4 mb-4">
+					<div class="flex justify-between items-center">
+						<h2 class="font-semibold">{item.title}</h2>
+						<button
+							aria-label="Remove item"
+							title="Remove item"
+							class="btn-icon variant-soft-error"
+						>
+							<i class="fa-regular fa-trash-can fa-lg" /></button
+						>
+					</div>
+					<div class="flex">
+						<div class="mt-1 w-full">
+							<span class="text-xs text-tertiary-400">Quantity</span>
+							<div class="flex justify-between items-center">
+								<div
+									class="flex items-center justify-between w-full max-w-[160px] h-14 px-2 border border-primary-300 bg-surface-50"
+								>
+									<button
+										class="btn-icon btn-icon-sm variant-soft-success"
+										aria-label="Decrement quantity"
+										title="Decrement quantity"
+									>
+										<i class="fa-solid fa-minus" />
+									</button>
+									<span>{item.quantity}</span>
+									<button
+										class="btn-icon btn-icon-sm variant-soft-success"
+										aria-label="Increment quantity"
+										title="Increment quantity"
+									>
+										<i class="fa-solid fa-plus" />
+									</button>
+								</div>
+								<div>${(item.price * item.quantity).toFixed(2)}</div>
+							</div>
+						</div>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
+	<div class="grid grid-cols-1 p-4 gap-4">
+		<div class="flex items-center justify-between">
+			<span class="text-lg">Total:</span>
+			<span class="text-lg">${cart.total.toFixed(2)}</span>
+		</div>
+		<button class="btn variant-filled-primary"
+			><span>Checkout</span><span><i class="fa-solid fa-arrow-right" /></span>
+		</button>
+		<a class="text-center underline underline-offset-4 text-primary-500 font-semibold" href="/cart"
+			>View detailed cart</a
+		>
+	</div>
+</section>
